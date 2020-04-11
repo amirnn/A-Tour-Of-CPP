0feeeea964d3b3e82f686f0da852a6745bc26491
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-11 23:06:21 +0200

    initial init.

---
+++ ch4-Classes/classHierarchies.cpp
@@ -0,0 +1,27 @@
+#include<utility>
+#include<vector>
+
+typedef unsigned uint;
+typedef std::pair<uint,uint> Point;
+
+class Shape {
+    public:
+    virtual Point center() const=0;
+    virtual void move(Point to)=0;
+    virtual void draw() const =0;
+    virtual void roatate(int angle)=0;
+    virtual ~Shape(){};
+};
+
+void roatateAll(std::vector<Shape*>& v, int angle){
+    for(auto p:v){
+        p->roatate(angle);
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
